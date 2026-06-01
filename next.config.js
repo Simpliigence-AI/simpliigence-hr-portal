b4bc75fc5a2bd23ed5318933d122522f950ca231
raghu-simplii <raghu@simpliigencesolutions.com>
2026-06-01 06:36:31 -0400

    fix: add nodemailer to serverExternalPackages in next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'cxfkwstpztxhkfknuqtj.supabase.co' },
     ],
   },
+  serverExternalPackages: ['nodemailer'],
 };
 
 module.exports = nextConfig;
